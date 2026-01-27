793dacd4e0d0bd65a2e96146920fbbe0fc800564
yassienE4 <yassienebrahim@gmail.com>
2026-01-27 21:40:15 +0200

    added more shadcn components

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page() {
+  
+
+  return (
+    <main className="p-8">
+      <h1 className="text-2xl font-bold">Home Page</h1>
+      <p className="mt-4">{"test"}</p>
+    </main>
+  );
+}
